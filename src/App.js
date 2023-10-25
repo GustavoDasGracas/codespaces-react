a00757c1514ed1d24e863ad9f20a07e4f42ef98d
Gspaiva57004 <gsantanpaiva123@gmail.com>
2023-10-25 11:08:12 -0300

    t

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          Gustavodas <span className="heart">♥️</span> React
+          Gustavo <span className="heart">♥️</span> React
         </p>
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
