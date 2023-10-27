ff4d95fbc38a1850bf26374986f1dcd4029df641
GustavoDasGracas <147611424+GustavoDasGracas@users.noreply.github.com>
2023-10-27 14:27:05 +0000

    teste

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import './App.css';
+import logo from './imagens/SENAI_São_Paulo_logo.png';
 
+import login from './imagens/imagemlogin.jpg'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GustavoG <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <br></br>
+        <img src={logo} classname="App-Logo" alt="Logo"></img>
+        <p>"Anchieta"</p>
 
-          b
-        </p>
-        
-      </header>
+        <ul className="Header-Menu">
+          <li className="Menu">Home </li>
+          <li className="Menu">Cursos</li>
+          <li className="menu">Turmas</li>
+        </ul>
+
+        <p>Fazer Login</p>
+        <img src={login} classname='Login' alt="login"/>
+       </header>
+
+      <section className="Corpo">
+       <nav className="Lateral">
+        <ul className="Menu-Lateral">
+          <li className="Menu-Lateral">Cursos Gratuitos</li>
+          <li className="Menu-Lateral">Cursos Tecnicos</li>
+          <li className="Menu-Lateral">Unidades</li>
+          <li className="Menu-Lateral">Fale Conosco</li>
+          <li className="Menu-Lateral">Fale Conosco</ui>
+        </ul>
+       </nav>
+
+       <article className="Pricipal">
+        <h1>Vestibular 2024</h1>
+        <p>Inscrições Abertas Na</p>
+        <h3>Faculdade De Indústrias</h3>
+        <p>03/10 a 04/12</p><br></br>
+
+        <h1>Cursos</h1>
+        <p>Pesquisar Todos</p>
+        <p>Administração e Gestão</p>
+        <p>Alimentos e Bebidas</p>
+       </article>
+      </section>
+
     </div>
   );
 }
